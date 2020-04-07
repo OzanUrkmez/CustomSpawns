27d152097f64f18712fbd3b31fcd815866d82f7c
Ozan Ürkmez <arkantos250@gmail.com>
2020-04-07 16:39:20 +0300

    one more potential idea

---
+++ Spawn/BanditFightEnslavementBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanditInfestation.Spawn
+{
+    class BanditFightEnslavementBehaviour //TODO see if bandits already do this at first
+    {
+    }
+}
