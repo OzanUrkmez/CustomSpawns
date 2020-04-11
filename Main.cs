9e279b8c4f198d17d87d58b81432bbc4070db9e1
Ozan Ürkmez <arkantos250@gmail.com>
2020-04-11 20:28:07 +0300

    update version

--- Main.cs
+++ Main.cs
@@ -15,7 +15,7 @@ namespace CustomSpawns
 {
     public class Main : MBSubModuleBase
     {
-        public static readonly string version = "v1.0.1";
+        public static readonly string version = "v1.0.2";
         public static CustomSpawnsCustomSpeedModel customSpeedModel;
 
         protected override void OnSubModuleLoad()
