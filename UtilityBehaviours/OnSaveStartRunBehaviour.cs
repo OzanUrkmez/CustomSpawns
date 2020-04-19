f00c32584a987db3e0d35ffa9819218f522055ff
Ozan Ürkmez <arkantos250@gmail.com>
2020-04-19 19:03:25 +0300

    add utility to run aa function after save has been lodaded

---
+++ UtilityBehaviours/OnSaveStartRunBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+using TaleWorlds.Localization;
+
+namespace CustomSpawns.UtilityBehaviours
+{
+    class OnSaveStartRunBehaviour : CampaignBehaviorBase
+    {
+        public override void RegisterEvents()
+        {
+            CampaignEvents.HourlyTickEvent.AddNonSerializedListener(this, HourlyBehaviour);
+        }
+
+        public override void SyncData(IDataStore dataStore)
+        {
+            
+        }
+
+        private Action run;
+        private bool alreadyRun = false;
+        private void HourlyBehaviour()
+        {
+            if (alreadyRun)
+                return;
+            alreadyRun = true;
+            run();
+        }
+
+        public void RegisterFunctionToRun(Action f)
+        {
+            run += f;
+        }
+    }
+}
