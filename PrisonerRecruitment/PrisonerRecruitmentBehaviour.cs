041f6e5d2c3dc18b5acf8cbb8b250a1959b5b76f
Ozan Ürkmez <arkantos250@gmail.com>
2020-04-24 11:55:35 +0300

    implement mobile party prisoner recruitment chance calculations

--- PrisonerRecruitment/PrisonerRecruitmentBehaviour.cs
+++ PrisonerRecruitment/PrisonerRecruitmentBehaviour.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
 using TaleWorlds.Localization;
+using TaleWorlds.TwoDimension;
 
 namespace CustomSpawns.PrisonerRecruitment
 {
@@ -21,17 +22,83 @@ namespace CustomSpawns.PrisonerRecruitment
 
         public override void RegisterEvents()
         {
-            CampaignEvents.DailyTickEvent.AddNonSerializedListener(this, DailyPrisonerRecruitmentEvent);
+            CampaignEvents.DailyTickPartyEvent.AddNonSerializedListener(this, DailyPrisonerRecruitmentEvent);
         }
 
         public override void SyncData(IDataStore dataStore)
         {
-           
+
         }
 
-        private void DailyPrisonerRecruitmentEvent()
+        private void DailyPrisonerRecruitmentEvent(MobileParty mb)
+        {
+            try
+            {
+                if (Config.Enabled == false)
+                    return;
+                if (!Config.ApplyToVanilla && !Utils.Utils.IsCustomSpawnsStringID(mb.StringId))
+                    return;
+                if (mb.Party.MapEvent != null)
+                    return;
+                float recruitChance = 0;
+                float devalueChance = 0;
+                int capTimes = 0;
+
+                Random rand = new Random();
+
+                recruitChance = Config.BaseRecruitChance;
+                devalueChance = Config.BaseDevalueChance;
+                recruitChance += Config.MercifulTraitModifier * mb.Leader.GetTraitLevel(DefaultTraits.Mercy);
+                devalueChance -= Config.MercifulTraitModifier * mb.Leader.GetTraitLevel(DefaultTraits.Mercy);
+                capTimes = (int)((float)mb.PrisonRoster.Count / ((float)mb.MemberRoster.Count * Config.PrisonerPartyPercentageCap));
+                recruitChance *= (float)Math.Pow(capTimes, Config.CapReverseFinalCoefficientPerCap);
+
+                List<CharacterObject> recruited = new List<CharacterObject>();
+                List<CharacterObject> devalued = new List<CharacterObject>();
+                foreach (CharacterObject c in mb.PrisonRoster.Troops)
+                {
+                    if (c.Culture != mb.Party.Culture)
+                    {
+                        recruitChance -= Config.DifferentCultureReverseModifier;
+                        devalueChance -= Config.DifferentCultureReverseModifier;
+                    }
+                    recruitChance -= c.Level * Config.PrisonerLevelReverseModifierPerLevel;
+                    devalueChance += c.Level * Config.PrisonerLevelDevalueModifierPerLevel;
+                    recruitChance = Math.Max(recruitChance, Config.FinalMinimumChance);
+                    devalueChance = Math.Max(devalueChance, Config.FinalMinimumChance);
+                    if(rand.NextDouble() <= recruitChance)
+                    {
+                        //recruit!
+                        recruited.Add(c);
+                    }
+                    if (rand.NextDouble() <= devalueChance)
+                    {
+                        //recruit!
+                        devalued.Add(c);
+                    }
+                }
+                recruited.ForEach((CharacterObject c) => PartyRecruitAndRemovePrisoner(mb, c));
+                devalued.ForEach((CharacterObject c) => PartyDevaluePrisoner(mb, c));
+            }
+            catch (Exception e)
+            {
+                ErrorHandler.HandleException(e, " daily prisoner recruitment event of " + mb.StringId);
+            }
+        }
+
+        private void PartyRecruitAndRemovePrisoner(MobileParty mb, CharacterObject c)
         {
 
+        }
+
+        private void PartyDevaluePrisoner(MobileParty mb, CharacterObject c)
+        {
+
+        }
+
+        private void DailyGarrisonEvent()
+        {
+            //TODO deal with garrisons.
         }
     }
 }
