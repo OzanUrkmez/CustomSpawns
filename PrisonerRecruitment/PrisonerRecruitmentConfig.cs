de74104f13f87b533453a815926bb1bccf1fb1f7
Ozan Ürkmez <arkantos250@gmail.com>
2020-04-24 10:32:19 +0300

    create config

---
+++ PrisonerRecruitment/PrisonerRecruitmentConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSpawns.PrisonerRecruitment
+{
+    [Serializable]
+    class PrisonerRecruitmentConfig
+    {
+
+        public bool Enabled { get; set; }
+        public float BaseRecruitChance { get; set; }
+        public float BaseDevalueChance { get; set; }
+        public float PrisonerLevelReverseModifierPerLevel { get; set; }
+        public float PrisonerLevelDevalueModifierPerLevel { get; set; }
+        public float MercifulTraitModifier { get; set; }
+        public float DifferentCultureReverseModifier { get; set; }
+        public float PrisonerPartyPercentageCap { get; set; }
+        public float CapReverseFinalCoefficientPerCap { get; set; }
+        public float GarrisonFinalCoefficient { get; set; }
+        public float FinalMinimumChance { get; set; }
+    }
+}
