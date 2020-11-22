efda862bca65a7137e5df09854c1baff911b2867
Ozan Ürkmez <arkantos250@gmail.com>
2020-11-22 22:31:57 +0300

    siege logs

--- CampaignData/Implementations/DailyLogger.cs
+++ CampaignData/Implementations/DailyLogger.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.Library;
 using TaleWorlds.TwoDimension;
 
@@ -42,6 +43,7 @@ namespace CustomSpawns.CampaignData {
             CampaignEvents.AfterDailyTickEvent.AddNonSerializedListener(this, OnAfterDailyTick);
             CampaignEvents.WarDeclared.AddNonSerializedListener(this, OnWarDeclared);
             CampaignEvents.MakePeace.AddNonSerializedListener(this, OnPeaceMade);
+            CampaignEvents.OnSettlementOwnerChangedEvent.AddNonSerializedListener(this, OnSettlementChange);
         }
 
 
@@ -85,13 +87,20 @@ namespace CustomSpawns.CampaignData {
             WriteString(fac1.Name + " and " + fac2.Name + " have made peace!\n");
         }
 
+        private void OnSettlementChange(Settlement s, bool b, Hero newOwner, Hero oldOwner, Hero h3, ChangeOwnerOfSettlementAction.ChangeOwnerOfSettlementDetail details)
+        {
+            if(details == ChangeOwnerOfSettlementAction.ChangeOwnerOfSettlementDetail.BySiege)
+            {
+                WriteString(s.Name + " has been captured succesfully through siege, changing hands from " + oldOwner.Clan.Kingdom.Name + " to " + newOwner.Clan.Kingdom.Name + "\n");
+            }
+        }
 
         public static void ReportSpawn(MobileParty spawned, float chanceOfSpawnBeforeSpawn)
         {
             if (Singleton == null || spawned.Party.TotalStrength < Singleton.campaignConfig.MinimumSpawnLogValue || chanceOfSpawnBeforeSpawn > Singleton.campaignConfig.MinimumRarityToLog)
                 return;
 
-            Singleton.WriteString("New Spawn: " + spawned.StringId + "\n    Total Strength:" + spawned.Party.TotalStrength.ToString() + "!\n");
+            Singleton.WriteString("New Spawn: " + spawned.StringId + "\n    Total Strength:" + spawned.Party.TotalStrength.ToString() + "\nChance of Spawn: " + chanceOfSpawnBeforeSpawn.ToString() +  "\n");
         }
 
         
