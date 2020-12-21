7bb40127debc6415c0cd6688f51850f5dd759f97
Ozan Ürkmez <arkantos250@gmail.com>
2020-12-21 13:30:49 +0300

    version number update

--- Main.cs
+++ Main.cs
@@ -18,9 +18,11 @@ namespace CustomSpawns
 {
     public class Main : MBSubModuleBase
     {
-        public static readonly string version = "v1.3.9";
+        public static readonly string version = "v1.4.0";
         public static readonly bool isAPIMode = false;
         public static CustomSpawnsCustomSpeedModel customSpeedModel;
+
+
 
         private static bool removalMode = false;
 
