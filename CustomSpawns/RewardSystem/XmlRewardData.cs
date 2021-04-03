30ff16df58d2045c88a73deb6b33b89732ed2ad3
Ozan Ürkmez <arkantos250@gmail.com>
2021-04-03 23:24:25 +0300

    rewards API loading hotfix

--- CustomSpawns/RewardSystem/XmlRewardData.cs
+++ CustomSpawns/RewardSystem/XmlRewardData.cs
@@ -32,67 +32,80 @@ namespace CustomSpawns.RewardSystem
 
         private void LoadDataFromXml()
         {
-            string pathToTamplate = "";
-            string pathToSchema = "";
-            if (!Main.isAPIMode)
-            {
-                pathToTamplate = Path.Combine(BasePath.Name, "Modules", "CustomSpawns", "ModuleData", "Data",
-                    "PartyRewards.xml");
-                pathToSchema = Path.Combine(BasePath.Name, "Modules", "CustomSpawns", "Schema",
-                    "PartyRewardTemplateSchema.xsd");
-            }
-            else
-            {
-                pathToTamplate = Path.Combine(BasePath.Name, "Modules", "CustomSpawnsCleanAPI", "ModuleData", "Data",
-                    "PartyRewards.xml");
-                pathToSchema = Path.Combine(BasePath.Name, "Modules", "CustomSpawnsCleanAPI", "Schema",
-                    "PartyRewardTemplateSchema.xsd");
-            }
-
-            // load xml schema for validation
             try
             {
-                using (var streamSchema = new StreamReader(pathToSchema))
-                {
-                    var schema = XmlSchema.Read(streamSchema, new ValidationEventHandler(ValidationEventHandler));
-                    var settings = new XmlReaderSettings();
-                    settings.Schemas.Add(schema);
-                    settings.ValidationType = ValidationType.Schema;
-                    // define settings for xml doc reading and validating
-                    using (var streamDocument = XmlReader.Create(pathToTamplate, settings))
-                    {
-                        // read the xml document
-                        var document = new XmlDocument();
-                        document.Load(streamDocument);
-                        // validate the xml document
-                        var eventHandler = new ValidationEventHandler(ValidationEventHandler);
-                        document.Validate(eventHandler);
 
-                        foreach (XmlNode partyRewardNode in document.DocumentElement)
-                        {
-                            if (partyRewardNode.NodeType == XmlNodeType.Comment)
-                                continue;
-                            string partyId;
-                            List<Reward> rewardsList;
-                            if (partyRewardNode.FirstChild.Name == "PartyId")
-                            {
-                                partyId = partyRewardNode.FirstChild.InnerText;
-                                rewardsList = RewardLoader(partyRewardNode.LastChild);
-                            }
-                            else
-                            {
-                                partyId = partyRewardNode.LastChild.InnerText;
-                                rewardsList = RewardLoader(partyRewardNode.FirstChild);
-                            }
-                            // add the new partyReword
-                            this.PartyRewards.Add(new PartyReward(partyId, rewardsList));
-                        }
-                    }
+                string pathToTamplate = "";
+                string pathToSchema = "";
+
+                if (!Main.isAPIMode)
+                {
+                    pathToTamplate = Path.Combine(BasePath.Name, "Modules", "CustomSpawns", "ModuleData", "Data",
+                        "PartyRewards.xml");
+                    pathToSchema = Path.Combine(BasePath.Name, "Modules", "CustomSpawns", "Schema",
+                        "PartyRewardTemplateSchema.xsd");
                 }
+                else
+                {
+                    pathToSchema = Path.Combine(BasePath.Name, "Modules", "CustomSpawnsCleanAPI", "Schema",
+                        "PartyRewardTemplateSchema.xsd");
+                    foreach (var subMod in ModIntegration.SubModManager.dependentModsArray)
+                    {
+                        string path = Path.Combine(subMod.CustomSpawnsDirectoryPath, "PartyRewards.xml");
+                        if (File.Exists(path))
+                            ParseRewardFile(pathToSchema, path);
+                    }
+
+                }
+
+
             }
             catch (Exception e)
             {
                 ErrorHandler.HandleException(e);
+            }
+
+        }
+
+        private void ParseRewardFile(string pathToSchema, string pathToTamplate)
+        {
+            // load xml schema for validation
+            using (var streamSchema = new StreamReader(pathToSchema))
+            {
+                var schema = XmlSchema.Read(streamSchema, new ValidationEventHandler(ValidationEventHandler));
+                var settings = new XmlReaderSettings();
+                settings.Schemas.Add(schema);
+                settings.ValidationType = ValidationType.Schema;
+                // define settings for xml doc reading and validating
+                using (var streamDocument = XmlReader.Create(pathToTamplate, settings))
+                {
+                    // read the xml document
+                    var document = new XmlDocument();
+                    document.Load(streamDocument);
+                    // validate the xml document
+                    var eventHandler = new ValidationEventHandler(ValidationEventHandler);
+                    document.Validate(eventHandler);
+
+                    foreach (XmlNode partyRewardNode in document.DocumentElement)
+                    {
+                        if (partyRewardNode.NodeType == XmlNodeType.Comment)
+                            continue;
+                        string partyId;
+                        List<Reward> rewardsList;
+                        if (partyRewardNode.FirstChild.Name == "PartyId")
+                        {
+                            partyId = partyRewardNode.FirstChild.InnerText;
+                            rewardsList = RewardLoader(partyRewardNode.LastChild);
+                        }
+                        else
+                        {
+                            partyId = partyRewardNode.LastChild.InnerText;
+                            rewardsList = RewardLoader(partyRewardNode.FirstChild);
+                        }
+                        // add the new partyReword
+                        this.PartyRewards.Add(new PartyReward(partyId, rewardsList));
+                    }
+                }
             }
 
         }
