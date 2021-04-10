e96328b0c98c1560ff35ccead92924069e4931af
Ozan Ürkmez <arkantos250@gmail.com>
2021-04-11 01:22:37 +0300

    update version

--- CustomSpawns/Main.cs
+++ CustomSpawns/Main.cs
@@ -19,7 +19,7 @@ namespace CustomSpawns
 {
     public class Main : MBSubModuleBase
     {
-        public static readonly string version = "v1.4.7";
+        public static readonly string version = "v1.4.8";
         public static readonly bool isAPIMode = false;
         public static CustomSpawnsCustomSpeedModel customSpeedModel;
 
